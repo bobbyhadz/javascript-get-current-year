86c7f3b6dace23d8b5e8657076b4e71dbcc45657
bobbyhadz <opmftw@gmail.com>
2024-03-03 15:39:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('bobbyhadz.com');
+
+// 👇️ Get current Year
+const currentYear = new Date().getFullYear();
+console.log(currentYear); // 👉️ 2023
+
+// ----------------------------------------------------
+
+// // Or define a reusable function
+
+// function getCurrentYear() {
+//   return new Date().getFullYear();
+// }
+
+// const currentYear = getCurrentYear();
+// console.log(currentYear); // 👉️ 2023
